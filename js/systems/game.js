e156f42ed05d4fd58636bd14e2bb747a83a9d93e
linomo <enter85331@gmail.com>
2026-02-06 23:15:21 +0800

    Update game.js

--- js/systems/game.js
+++ js/systems/game.js
@@ -16,8 +16,8 @@ const player = {
   mood: 50,
   stress: 25,
   money: 100,
-  currentEP:,  // 當前元氣
-  maxEP:,      // 最大元氣
+  currentEP: 60,  // 當前元氣
+  maxEP: 60,      // 最大元氣
   dirtiness: 0,
   favor: { SF: 20, SS: 10, DS: 0 },
   
