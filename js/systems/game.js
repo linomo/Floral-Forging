a5b7b9c25aaf151ea52cdbb51c2eda17e9f1eb6d
linomo <enter85331@gmail.com>
2026-02-06 23:14:08 +0800

    Update game.js

--- js/systems/game.js
+++ js/systems/game.js
@@ -17,7 +17,7 @@ const player = {
   stress: 25,
   money: 100,
   currentEP: maxEP,  // 當前元氣
-  maxEP: 2 * (player.int + player.dex + player.str) / 3),      // 最大元氣
+  maxEP:  Math.floor(2 * (player.int + player.dex + player.str) / 3),      // 最大元氣
   dirtiness: 0,
   favor: { SF: 20, SS: 10, DS: 0 },
   
