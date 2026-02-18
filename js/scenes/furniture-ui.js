0f7e409143bd50b4a0f5db971d887103026b4396
linomo <enter85331@gmail.com>
2026-02-18 21:56:01 +0800

    Update furniture-ui.js

--- js/scenes/furniture-ui.js
+++ js/scenes/furniture-ui.js
@@ -171,7 +171,7 @@ const FurnitureUI = {
                     <div class="furniture-detail-icon">${furniture.icon}</div>
                     <div class="furniture-detail-name">${furniture.name}</div>
                     <div class="furniture-detail-effect">${effectText}</div>
-                    <div class="furniture-item-cost">維護費 ${parseInt(f.maintenance) || 0}元/旬</div>
+                    <div class="furniture-item-cost">維護費 ${parseInt(furniture.maintenance) || 0}元/旬</div>
                 </div>
                 <button class="furniture-btn secondary" onclick="FurnitureUI.openMoveModal('${furnitureId}')">
                     🔄 移動位置
