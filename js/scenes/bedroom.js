b7fc2d4b66474d6da945e6addc9cae430ad57701
linomo <enter85331@gmail.com>
2026-02-19 17:26:40 +0800

    Update bedroom.js

--- js/scenes/bedroom.js
+++ js/scenes/bedroom.js
@@ -8,7 +8,7 @@ const BedroomScene = {
     renderHeader() {
         const expandStatus = player.roomExpanded ? '（已擴建）' : '';
         return `
-            <span style="font-weight:bold;font-size:1.1em">🏠 小哈的房間 ${expandStatus}</span>
+            <span style="font-weight:bold;font-size:1.1em">🏠 小房間 ${expandStatus}</span>
             <span style="margin-left:15px;color:#888">
                 🛋️ 家具：<span style="color:#f5a623;font-weight:bold">${this._countPlacedFurniture()}</span> 個
             </span>`;
